6e73c1444626b921fb37a1be9e0df520097bcf7b
Johanne Khan Malek <johannekhanmalek@johannes-mbp.lan>
2023-11-27 17:10:51 +0100

    accepted conflict resolution

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // Pages import
 import Selection from "./pages/Selection";
 
-
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 // Pages import
@@ -12,22 +11,13 @@ import Selection from "./pages/Selection";
 import ProductDescription from "./Pages/ProductDescription";
 
 function App() {
-<<<<<<< HEAD
-  return <Router>
-    <Routes>
-      <Route path="/" element={<Selection/>}></Route>
-    </Routes>
-  </Router>;
-=======
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Selection />}></Route>
-        <Route path="/products/:id" element={<ProductDescription />} />
       </Routes>
     </Router>
   );
->>>>>>> 98f6877f45d44ebbdaba85978c3f380ab13cf47c
 }
 
 export default App;
