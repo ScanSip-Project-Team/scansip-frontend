98f6877f45d44ebbdaba85978c3f380ab13cf47c
Zamua Halera <zamuahalera@mbp-de-zamua.lan>
2023-11-27 16:58:11 +0100

    regenerate yarn lock

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import "./App.css";
+
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
-//Pages
+// Pages import
+import Selection from "./pages/Selection";
 import ProductDescription from "./Pages/ProductDescription";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<ProductDescription />} />
+        <Route path="/" element={<Selection />}></Route>
+        <Route path="/products/:id" element={<ProductDescription />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/Selection.jsx
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+
+const Selection = () => {
+
+
+
+    return <div>
+        <p>Page Selection</p>
+        <div className="flex-parent">
+
+        </div>
+        <input 
+        type="submit"
+        value="Valider le panier" />
+    </div>
+};
+
+export default Selection;
