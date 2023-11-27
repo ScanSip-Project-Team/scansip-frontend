2301a22e8d932bb26cc12459cb3fa34932846037
ThibaultTrofl <86740430+ThibaultTrofl@users.noreply.github.com>
2023-11-27 16:45:06 +0100

    Add flashScreen

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 //Pages
 import ProductDescription from "./Pages/ProductDescription";
+import FlashScreen from "./pages/FlashScreen/FlashScreen";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<ProductDescription />} />
+        <Route path="/flashscreen" element={<FlashScreen/>}/>
       </Routes>
     </Router>
   );
---
+++ src/pages/FlashScreen/FlashScreen.jsx
@@ -0,0 +1,10 @@
+// Import Style
+import logo from "../../assets/logo.svg"
+const FlashScreen=()=>{
+
+    return<main className="w-screen h-screen justify-center items-center flex">
+<img src={logo} alt="Logo carré avec une ecriture ScanSip verte sur un fond noir"/>
+    </main>
+}
+
+export default FlashScreen
