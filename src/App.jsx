178a1e60fbcce23beea6d715930ae870569b34c6
Yohann Andre <y.andre90000@gmail.com>
2023-11-27 16:23:33 +0100

    add tailwind react router and react navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+//Pages
+import ProductDescription from "./Pages/ProductDescription";
 
 function App() {
-  return <></>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<ProductDescription />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
