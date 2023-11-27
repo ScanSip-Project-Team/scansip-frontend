cd0fabf1b0163a0ee46d5a8d3240f9a30cb838ce
Yohann Andre <y.andre90000@gmail.com>
2023-11-27 18:26:57 +0100

    add modal component

--- src/pages/Selection.jsx
+++ src/pages/Selection.jsx
@@ -1,38 +1,38 @@
 import axios from "axios";
-import {useEffect, useState} from "react";
-
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const Selection = () => {
-    const [data,setData]= useState();
-    const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState();
+  const [isLoading, setIsLoading] = useState(true);
 
-    useEffect(()=> {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://site--scansip-backend--jswmm7jk2mlr.code.run/products"
+        );
+        console.log("response.data >>>>", response.data);
+        setData(response.data);
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
 
-        const fetchData = async ()=> {
-            try {
-                const response = await axios.get("https://site--scansip-backend--jswmm7jk2mlr.code.run/products");
-                console.log("response.data >>>>", response.data);
-                setData(response.data);
-                setIsLoading(false);
-            }catch(error) {console.log(error)};
-        };
-        fetchData();
+  return isLoading ? (
+    <p>Loading...</p>
+  ) : (
+    <div>
+      <p>Page Selection</p>
+      <div className="flex-parent"></div>
+      <input type="submit" value="Valider le panier" />
 
-
-
-    }, [])
-
-
-
-    return isLoading ? <p>Loading...</p> : (<div>
-        <p>Page Selection</p>
-        <div className="flex-parent">
-
-        </div>
-        <input 
-        type="submit"
-        value="Valider le panier" />
-    </div>)
+      <Link to={"/products"}>Modal Yohann</Link>
+    </div>
+  );
 };
 
 export default Selection;
