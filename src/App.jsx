38a3ec5f8930e424874158a399f5d3e7ecc51c1f
Johanne Khan Malek <johannekhanmalek@johannes-mbp.lan>
2023-11-27 16:23:03 +0100

    add page Selection

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// Pages import
+import Selection from "./pages/Selection";
+
 
 function App() {
-  return <></>;
+  return <Router>
+    <Routes>
+      <Route path="/" element={<Selection/>}></Route>
+    </Routes>
+  </Router>;
 }
 
 export default App;
