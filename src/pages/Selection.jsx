6d567b3dffb7a468cee28ea1799c918d444d3bc4
Johanne Khan Malek <johannekhanmalek@johannes-mbp.lan>
2023-11-28 11:52:04 +0100

    selection banner

--- src/pages/Selection.jsx
+++ src/pages/Selection.jsx
@@ -23,14 +23,36 @@ const Selection = () => {
     fetchData();
   }, []);
 
-    return isLoading ? <p>Loading...</p> : (<div>
-        <p>Page Selection</p>
-        <p>Just another line</p>
+    return isLoading ? <p>Loading...</p> : (<div className="w-screen flex justify-center items-center flex-col">
+      
+       
         
-        {data.map((item)=> {
-            return <div key={item._id}> 
-            <p>{item.product_name}</p></div>
-        })}
+        {/* {data.map((item)=> {
+            return <div key={item._id} className="justify-center inline-flex"> 
+            <p>{item.product_category}</p>
+            
+            </div>
+        })} */}
+
+        <div className="flex items-center justify-center gap-2 w-11/12 border-black border-2 my-6">
+            <div className="flex flex-col h-70 w-1/4 bg-[#F3F3F3] items-center rounded p-1.5">
+                <img className="h-12 w-8"src="../src/assets/soft.png" alt="soft" />
+                
+                <p>Soft</p>
+            </div>
+            <div className="flex flex-col h-70 w-1/4 bg-[#F3F3F3] items-center rounded p-1.5">
+            <img className="h-12 w-8"src="../src/assets/HotDog.png" alt="snacks" />
+                <p>Snacks</p>
+            </div>
+            <div className="flex flex-col h-70 w-1/4 bg-[#F3F3F3] items-center rounded p-1.5">
+            <img className="h-12 w-8"src="../src/assets/biere.png" alt="beer" />
+            <p>Alcools</p>
+            </div>
+            <div className="flex flex-col h-70 w-1/4 bg-[#F3F3F3] items-center rounded p-1.5">
+            <img className="h-12 w-8"src="../src/assets/Cocktail.png" alt="cocktail" />
+            <p>Cocktails</p>
+            </div>
+        </div>
         <div className="flex-parent"></div>
         <input 
         type="submit"
