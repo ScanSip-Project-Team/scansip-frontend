6335ba098676fd1d72dac993bbd381ab65810948
Johanne Khan Malek <johannekhanmalek@johannes-mbp.lan>
2023-12-06 12:34:37 +0100

    bigger buttons grey border

--- src/components/ButtonQuantity.jsx
+++ src/components/ButtonQuantity.jsx
@@ -2,7 +2,7 @@ const ButtonQuantity = ({ text, func }) => {
   return (
     <>
       <button
-        className="h-[28px]  w-[28px] rounded-[20px] bg-[#E8E8E8] text-[10px] "
+        className="h-[32px]  w-[32px] rounded-[20px]  bg-[#E8E8E8] text-[10px] "
         onClick={() => func()}
       >
         {text}
