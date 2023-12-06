30e0d47ff6d7752f87dbaeac7b7feea5168ddb77
SamHalera <samuel.halera@gmail.com>
2023-12-07 00:57:56 +0100

    minor changes css loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,10 +1,11 @@
 import loaderGif from "../assets/loader.gif";
 const Loader = () => {
   return (
-    <div className="flex h-screen w-screen flex-col items-center justify-center ">
-      <div className="">
-        <img className="w-60" src={loaderGif} alt="" />
+    <div className="flex  h-96  w-screen flex-col items-center justify-center ">
+      <div className="mb-3">
+        <img className=" w-36  md:w-44  " src={loaderGif} alt="" />
       </div>
+      <p className="primary-color text-bold text-xl">Loading ...</p>
     </div>
   );
 };
