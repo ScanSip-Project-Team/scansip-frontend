2e407ca19716734b1f46386912ec2c5cb394b9dd
ThibaultTrofl <86740430+ThibaultTrofl@users.noreply.github.com>
2023-12-06 16:22:48 +0100

    Payement redirection rename

--- src/App.jsx
+++ src/App.jsx
@@ -67,7 +67,7 @@ function App() {
           }
         />
         <Route path="/billing/:id" element={<Billing />} />
-        <Route path="/paiement/:id" element={<UserPaiement total={total} />} />
+        <Route path="/payement/:id" element={<UserPaiement total={total} />} />
 
         <Route
           path="/cart"
