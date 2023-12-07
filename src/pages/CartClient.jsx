a01535ee5cace80272d96b90eded94c6586d1ed5
Johanne Khan Malek <johannekhanmalek@johannes-mbp.lan>
2023-12-07 12:47:50 +0100

    cartclient removed fixed position for button

--- src/pages/CartClient.jsx
+++ src/pages/CartClient.jsx
@@ -97,7 +97,7 @@ const CartClient = ({ setCart, cart, setTotal, total }) => {
   return (
     // <section className="flex w-screen flex-col ">
     <main className="flex h-screen w-screen flex-col items-center">
-      <div className="border-lightgrey fixed top-0 flex w-screen items-center justify-center  gap-2 border-b bg-white ">
+      <div className="border-lightgrey fixed top-0 flex w-screen items-center justify-center  gap-2    bg-white">
         <div className="flex w-screen flex-col items-center justify-center gap-2 bg-white">
           <nav className="mx-[10px] mt-[15px] self-start">
             <BreadCrumb
@@ -106,13 +106,13 @@ const CartClient = ({ setCart, cart, setTotal, total }) => {
             />
           </nav>
           <ShopName />
-          <div className="mb-3 flex justify-center">
+          <div className="mb-3 flex justify-center ">
             <p className="font-bold">Mon panier</p>
           </div>
         </div>
       </div>
 
-      <div className="mb-24 mt-32 flex w-screen flex-col items-center pb-[140px]">
+      <div className=" mt-2 flex w-screen flex-col items-center   pb-[20px]">
         <ListProduct
           data={cart}
           cart={cart}
@@ -121,17 +121,17 @@ const CartClient = ({ setCart, cart, setTotal, total }) => {
           total={total}
         />
       </div>
-      <div className="border-lightgrey  fixed bottom-0 mx-[10px] flex w-screen flex-col  items-center  gap-2.5 border-t bg-white py-6">
+      <div className="border-lightgrey   mx-[10px] flex w-screen flex-col  items-center  gap-2.5 border-t bg-white py-6">
         <Button
           text={`Commande pour un total de ${total} €`}
-          className={"btn-client w-available  mx-[10px] bg-black text-white"}
+          className={"btn-client mx-[10px]  w-available bg-black text-white"}
           func={Cookies.get("orderToModify") ? modifyOrder : createOrder}
           disabled={isDisabled}
         />
         <Button
           text={"Ajouter des articles"}
           className={
-            "btn-client w-available bg-greyAddArticlesButton mx-[10px] text-black"
+            "btn-client mx-[10px] w-available bg-greyAddArticlesButton text-black"
           }
           func={handleNavigateToHome}
           disabled={isDisabled}
